a8fd9d84a6fa8fec0e4f921d0ce46df84df6a6b3
FarshidMousavi <r.shidef@gmail.com>
2022-04-24 13:43:02 +0430

    Revert "add scrapy for speed"

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
 
 url = "https://api.divar.ir/v8/search/1/residential-rent"
 json = {"json_schema":{"category":{"value":"residential-rent"},"districts":{"vacancies":["195","654"]}},"last-post-date":1650519315954570}
@@ -30,6 +34,40 @@ with open ("token.txt" , "w" , encoding="utf-8") as file :
     file.write(",".join(list_of_tokens))
 
 
+url_list = [ f"https://divar.ir/v/-/{token}" for token in list_of_tokens]
+
+result = []
+n =1
+for url in url_list :
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content , "html.parser")
+    information = soup.find_all("span" , class_="kt-group-row-item__value")
+    price = soup.find_all("p" , class_ ="kt-unexpandable-row__value")
+    desciption = soup.find("p" , class_ = "kt-description-row__text post-description kt-description-row__text--primary").getText()
+    deposit = price[0].getText()
+    rent = price[1].getText()
+    area = information[0].getText()
+    construction =   information[1].getText()
+    rooms = information[2].getText()
+    elevator = False if "ندارد" in information[3].getText() else True
+    pariking = False if "ندارد" in information[4].getText() else True
+    warehouse = False if "ندارد" in information[5].getText() else True
+
+    result.append({
+        "deposit": deposit,
+        "rent" : rent ,
+        "area" : area ,
+        "construction" : construction,
+        "rooms" : rooms ,
+        "elevator" : elevator ,
+        "pariking" : pariking ,
+        "warehouse" : warehouse,
+        "desciption" : desciption,
+    })
+    print ( n ,f"Crawling {url}" )
+    n +=1
+df = pd.DataFrame(result)
+df.to_csv("result.csv" , encoding="utf-8-sig")   
 
 
     
--- readme.md
+++
@@ -1,6 +0,0 @@
-first run main.py to take tokens
-
-then run command :
-scrapy runspider crawler.py -o result.csv
-
-to export csv
